b4d2688cefd9f78266aea86ca62916f2738a31f8
Luna Das <cse2013luna@gmail.com>
2019-09-05 17:58:33 +0530

    Add reverse shuffle and merge two strings return lexicographical smallest.

---
+++ reverse_shuffle_merge/reverse_shuffle_merge.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+S = input()
+S = S[::-1]
+count = defaultdict(int)
+for c in S:
+    count[c] += 1
+need = {}
+for c in count:
+    need[c] = count[c] / 2
+solution = []
+i = 0
+while len(solution) < len(S) / 2:
+    min_char_at = -1
+    while True:
+        c = S[i]
+        if need[c] > 0 and (min_char_at < 0 or c < S[min_char_at]):
+            min_char_at = i
+        count[c] -= 1
+        if count[c] < need[c]:
+            break
+        i += 1
+    for j in range(min_char_at+1, i+1):
+        count[S[j]] += 1
+    need[S[min_char_at]] -= 1
+    solution.append(S[min_char_at])
+    i = min_char_at + 1
+print(''.join(solution))
